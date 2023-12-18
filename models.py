41c3cac30f44b4070e87f1acf44999d1d48cb247
Giulio <153623095+venerableDodo@users.noreply.github.com>
2023-12-18 19:30:47 +0100

    Small changes in dictionary file

--- models.py
+++ models.py
@@ -2,14 +2,23 @@
 
 # ! NEEDS TO BE COMPLETED
 
+# Declare Dictionary
 
-models = {
-    "Energy": "/define/models energy\nyes\nno\nno\nno\nyes\n",
-    "Viscous": {
-        "Laminar": "/define/models/viscous laminar\nyes",
-        "ke-realizable": "/define/models/viscous ke-realizable\nyes\n/define/models/viscous/near-wall-treatment enhanced-wall-treatment\nyes\n",
-    },
-    "Radiation": {
-        "DO": "/define/models/radiation discrete-ordinates\nyes\n4\n4\n4\n4\n",
-    },
+models = {}
+
+# Viscosity Models
+
+models["Viscous"] = {
+    "Laminar": "/define/models/viscous laminar\nyes",
+    "ke-realizable": "/define/models/viscous ke-realizable\nyes\n/define/models/viscous/near-wall-treatment enhanced-wall-treatment\nyes\n",
 }
+
+# Radiation Models
+
+models["Radiation"] = {
+    "DO": "/define/models/radiation discrete-ordinates\nyes\n4\n4\n4\n4\n",
+}
+
+# Energy 
+
+models["Energy"] = "/define/models energy\nyes\nno\nno\nno\nyes\n"
